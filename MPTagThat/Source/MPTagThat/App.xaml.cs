c117cdd988bd54ecc56021c620db83cb940eb270
Helmut Wahrmann <hwahrmann@team-mediaportal.com>
2017-09-04 18:14:10 +0200

    Startup processing and checking of command line args

--- MPTagThat/Source/MPTagThat/App.xaml.cs
+++ MPTagThat/Source/MPTagThat/App.xaml.cs
@@ -1,11 +1,14 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Windows;
 
 namespace MPTagThat
 {
   /// <summary>
   /// Interaction logic for App.xaml
   /// </summary>
-  public partial class App : Application
+  public partial class App
   {
     /// <summary>
     /// Instantiate the Unity Bootstrapper
@@ -14,8 +17,54 @@ namespace MPTagThat
     protected override void OnStartup(StartupEventArgs e)
     {
       base.OnStartup(e);
-      Bootstrapper bootstrapper = new Bootstrapper();
+
+      try
+      {
+        // We need to set the app.config file programmatically to point to the users APPDATA Folder
+        var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        var settings = configFile.AppSettings.Settings;
+        var key = "Raven/WorkingDir";
+        var value = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                    "\\MPTagthat\\Databases";
+        if (settings[key] == null)
+        {
+          settings.Add(key, value);
+        }
+        else
+        {
+          settings[key].Value = value;
+        }
+        configFile.Save(ConfigurationSaveMode.Modified);
+        ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+      }
+      catch (ConfigurationErrorsException)
+      {
+      }
+
+      // Need to reset the Working directory, since when we called via the Explorer Context menu, it'll be different
+      Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+      // Add our Bin and Bin\Bass Directory to the Path
+      SetPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Bin"));
+      SetPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Bin\Bass"));
+
+      // Call Bootstrapper with command line arguments
+      Bootstrapper bootstrapper = new Bootstrapper(e.Args);
       bootstrapper.Run();
     }
+
+    #region Private Methods
+
+    /// <summary>
+    /// Set the Path for the binaries
+    /// </summary>
+    /// <param name="path"></param>
+    private static void SetPath(string path)
+    {
+      var newPath = $"{Environment.GetEnvironmentVariable("Path")};{path}";
+      Environment.SetEnvironmentVariable("Path", newPath);
+    }
+
+    #endregion
   }
 }
--- MPTagThat/Source/MPTagThat/Bootstrapper.cs
+++ MPTagThat/Source/MPTagThat/Bootstrapper.cs
@@ -16,11 +16,17 @@
 // along with MPTagThat. If not, see <http://www.gnu.org/licenses/>.
 #endregion
 
-#region 
+#region
 
+using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+using System.Threading;
 using System.Windows;
-using Microsoft.Practices.ServiceLocation;
+using System.Xml;
 using Microsoft.Practices.Unity;
+using MPTagThat.Core.Settings;
 using MPTagThat.Services.Logging;
 using NLog;
 using Prism.Unity;
@@ -37,6 +43,26 @@ namespace MPTagThat
   /// </summary>
   public class Bootstrapper : UnityBootstrapper
   {
+    #region Variables
+
+    private string[] _commandLineArgs;
+    private static StartupSettings _startupSettings;
+    private static int _portable;
+    private static string _startupFolder;
+
+    #endregion
+
+    #region ctor
+
+    public Bootstrapper(string[] args)
+    {
+      _commandLineArgs = args;
+    }
+
+    #endregion
+
+    #region Overrides
+
     /// <summary>
     /// Resolve our main Shell Winndow
     /// </summary>
@@ -53,6 +79,68 @@ namespace MPTagThat
     {
       var log = Container.Resolve<ILogger>().GetLogger;
       var settings = Container.Resolve<ISettingsManager>("SettingsManager");
+
+      _portable = 0;
+      _startupFolder = "";
+      // Process Command line Arguments
+      foreach (string arg in _commandLineArgs)
+      {
+        if (arg.ToLower().StartsWith("/folder="))
+        {
+          _startupFolder = arg.Substring(8);
+        }
+        else if (arg.ToLower() == "/portable")
+        {
+          _portable = 1;
+        }
+      }
+
+      try
+      {
+        // Let's see, if we already have an instance of MPTagThat open
+        using (var mmf = MemoryMappedFile.OpenExisting("MPTagThat"))
+        {
+          if (_startupFolder == string.Empty)
+          {
+            // Don't allow a second instance of MPTagThat running
+            return;
+          }
+
+          byte[] buffer = Encoding.Default.GetBytes(_startupFolder);
+          var messageWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, "MPTagThat_IPC");
+
+          // Create accessor to MMF
+          using (var accessor = mmf.CreateViewAccessor(0, buffer.Length))
+          {
+            // Write to MMF
+            accessor.WriteArray(0, buffer, 0, buffer.Length);
+            messageWaitHandle.Set();
+
+            // End exit this instance
+            return;
+          }
+        }
+      }
+      catch (FileNotFoundException)
+      {
+        // The Memorymap does not exist, so MPTagThat is not yet running
+      }
+
+      log.Info("MPTagThat is starting...");
+
+      // Read the Config file
+      ReadConfig();
+
+      settings.StartSettings = _startupSettings;
+
+      // Move Init of Services, which we don't need immediately to a separate thread to increase startup performance
+      Thread initService = new Thread(() => DoInitService(Container))
+      {
+        IsBackground = true,
+        Name = "InitService"
+      };
+      initService.Start();
+
       Application.Current.MainWindow.Show();
     }
 
@@ -75,5 +163,146 @@ namespace MPTagThat
       ModuleCatalog catalog = (ModuleCatalog)ModuleCatalog;
       // Need to add the module catalogs here
     }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Init Service Thread
+    /// </summary>
+    private static void DoInitService(IUnityContainer container)
+    {
+     byte[] buffer = new byte[2048];
+      var messageWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, "MPTagThat_IPC");
+
+      container.Resolve<ILogger>().GetLogger.Debug("Registering MemoryMappedFile");
+      // Create named MMF
+      using (var mmf = MemoryMappedFile.CreateOrOpen("MPTagThat", 2048))
+      {
+        // Create accessor to MMF
+        using (var accessor = mmf.CreateViewAccessor(0, buffer.Length))
+        {
+          // Wait for the Message to be fired
+          while (true)
+          {
+            container.Resolve<ILogger>().GetLogger.Debug("Wait for Startup Event to be fired");
+            messageWaitHandle.WaitOne();
+            container.Resolve<ILogger>().GetLogger.Debug("Startup Event fired");
+
+            // Read from MMF
+            accessor.ReadArray(0, buffer, 0, buffer.Length);
+
+            string startupFolder = Encoding.Default.GetString(buffer).Trim(' ', '\x00');
+            //SetCurrentFolder(startupFolder);
+          }
+        }
+      }
+    }
+
+    /// <summary>
+    /// Read the Config.xml file
+    /// </summary>
+    private static void ReadConfig()
+    {
+      string configFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config.xml");
+      if (!File.Exists(configFile))
+        return;
+
+      _startupSettings = new StartupSettings();
+      try
+      {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(configFile);
+
+        // Check, if we got a config.xml
+        if (doc.DocumentElement == null) return;
+        string strRoot = doc.DocumentElement.Name;
+        if (strRoot != "config") return;
+
+        XmlNode portableNode = doc.DocumentElement.SelectSingleNode("/config/portable");
+        if (portableNode != null)
+        {
+          if (_portable == 0)
+          {
+            // Only use the value from Config, if not overriden by an argument
+            _portable = Convert.ToInt32(portableNode.InnerText);
+          }
+          _startupSettings.Portable = _portable != 0;
+        }
+
+        XmlNode maxSongsNode = doc.DocumentElement.SelectSingleNode("/config/MaximumNumberOfSongsInList");
+        _startupSettings.MaxSongs = maxSongsNode != null ? Convert.ToInt32(maxSongsNode.InnerText) : 500;
+
+        XmlNode ravenDebugNode = doc.DocumentElement.SelectSingleNode("/config/RavenDebug");
+        _startupSettings.RavenDebug = ravenDebugNode != null && Convert.ToInt32(ravenDebugNode.InnerText) != 0;
+
+        XmlNode ravenStudioNode = doc.DocumentElement.SelectSingleNode("/config/RavenStudio");
+        _startupSettings.RavenStudio = ravenStudioNode != null && Convert.ToInt32(ravenStudioNode.InnerText) != 0;
+
+        XmlNode ravenPortNode = doc.DocumentElement.SelectSingleNode("/config/RavenStudioPort");
+        _startupSettings.RavenStudioPort = ravenPortNode != null ? Convert.ToInt32(ravenPortNode.InnerText) : 8080;
+
+        XmlNode ravenDatabaseNode = doc.DocumentElement.SelectSingleNode("/config/MusicDatabaseFolder");
+        var dbPath = ravenDatabaseNode?.InnerText ?? "%APPDATA%\\MPTagThat\\Databases";
+        dbPath = CheckPath(dbPath);
+        _startupSettings.DatabaseFolder = dbPath;
+
+        XmlNode coverArtNode = doc.DocumentElement.SelectSingleNode("/config/CoverArtFolder");
+        var coverArtPath = coverArtNode?.InnerText ?? "%APPDATA%\\MPTagThat\\CoverArt";
+        coverArtPath = CheckPath(coverArtPath);
+        _startupSettings.CoverArtFolder = coverArtPath;
+      }
+      catch (Exception)
+      {
+        // ignored
+      }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="strPath"></param>
+    /// <returns></returns>
+    private static string CheckPath(string strPath)
+    {
+      string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+      strPath = strPath.Replace("%APPDATA%", appData);
+      strPath = strPath.Replace("%AppData%", appData);
+      string commonData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+      strPath = strPath.Replace("%PROGRAMDATA%", commonData);
+      strPath = strPath.Replace("%ProgramData%", commonData);
+
+      // Check to see, if the location was specified with an absolute or relative path.
+      // In case of relative path, prefix it with the startuppath   
+      if (!Path.IsPathRooted(strPath))
+      {
+        strPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, strPath);
+      }
+
+      // Create the folder
+      if (!Directory.Exists(strPath))
+      {
+        try
+        {
+          Directory.CreateDirectory(strPath);
+        }
+        catch (Exception)
+        {
+          // ignored
+        }
+      }
+
+      // See if we got a slash at the end. If not add one.
+      if (!strPath.EndsWith(@"\"))
+      {
+        strPath += @"\";
+      }
+
+      return strPath;
+    }
+
+
+    #endregion
   }
 }
