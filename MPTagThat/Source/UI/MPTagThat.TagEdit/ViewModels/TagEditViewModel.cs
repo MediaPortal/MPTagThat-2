40ad8780a2e7a2d85c8efd215cacedada1e2b4cc
Helmut Wahrmann <hwahrmann@team-mediaportal.com>
2019-12-06 13:21:22 +0100

    Handling of Checkboxes in Multiple Tag Edit

--- MPTagThat/Source/UI/MPTagThat.TagEdit/ViewModels/TagEditViewModel.cs
+++ MPTagThat/Source/UI/MPTagThat.TagEdit/ViewModels/TagEditViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 using MPTagThat.Core;
 using MPTagThat.Core.Common;
@@ -18,6 +19,8 @@ namespace MPTagThat.TagEdit.ViewModels
   public class TagEditViewModel : BindableBase, INavigationAware
   {
     #region Variables
+
+    private List<SongData> _songs = null;
 
     #endregion
 
@@ -38,47 +41,128 @@ namespace MPTagThat.TagEdit.ViewModels
     }
 
     // Check Box Checked properties
-    public bool CkTrackIsChecked { get; set; }
-    public bool CkDiskIsChecked { get; set; }
-    public bool CkArtistIsChecked { get; set; }
-    public bool CkAlbumArtistIsChecked { get; set; }
-    public bool CkGenreIsChecked { get; set; }
-    public bool CkAlbumIsChecked { get; set; }
-    public bool CkTitleIsChecked { get; set; }
-    public bool CkYearIsChecked { get; set; }
-    public bool CkBPMIsChecked { get; set; }
-    public bool CkConductorIsChecked { get; set; }
-    public bool CkComposerIsChecked { get; set; }
-    public bool CkInterpretedByIsChecked { get; set; }
-    public bool CkTextWriterIsChecked { get; set; }
-    public bool CkPublisherIsChecked { get; set; }
-    public bool CkEncodedByIsChecked { get; set; }
-    public bool CkCopyrightIsChecked { get; set; }
-    public bool CkContentGroupIsChecked { get; set; }
-    public bool CkSubTitleIsChecked { get; set; }
-    public bool CkArtistSortIsChecked { get; set; }
-    public bool CkAlbumSortIsChecked { get; set; }
-    public bool CkTitleSortIsChecked { get; set; }
-    public bool CkOriginalAlbumIsChecked { get; set; }
-    public bool CkOriginalArtistIsChecked { get; set; }
-    public bool CkOriginalFileNameIsChecked { get; set; }
-    public bool CkOriginalLyricsWriterIsChecked { get; set; }
-    public bool CkOriginalOwnerIsChecked { get; set; }
-    public bool CkOriginalReleaseIsChecked { get; set; }
-    public bool CkCopyrightUrlIsChecked { get; set; }
-    public bool CkOfficialAudioFileUrlIsChecked { get; set; }
-    public bool CkOfficialArtistUrlIsChecked { get; set; }
-    public bool CkOfficialAudioSourceUrlIsChecked { get; set; }
-    public bool CkOfficialInternetRadioUrlIsChecked { get; set; }
-    public bool CkOfficialPaymentUrlIsChecked { get; set; }
-    public bool CkCommercialInformationUrlIsChecked { get; set; }
-    public bool CkOfficialPublisherUrlIsChecked { get; set; }
-    public bool CkInvolvedPersonIsChecked { get; set; }
-    public bool CkInvolvedMusicianIsChecked { get; set; }
-    public bool CkRemoveExistingRatingsIsChecked { get; set; }
-    public bool CkRemoveLyricsIsChecked { get; set; }
-    public bool CkMediaTypeIsChecked { get; set; }
-    public bool CkTrackLengthIsChecked { get; set; }
+    private bool _ckTrackIsChecked;
+    public bool CkTrackIsChecked { get => _ckTrackIsChecked; set => SetProperty(ref _ckTrackIsChecked, value); }
+
+    private bool _ckDiscIsChecked;
+    public bool CkDiscIsChecked { get => _ckDiscIsChecked; set => SetProperty(ref _ckDiscIsChecked, value); }
+
+    private bool _ckArtistIsChecked;
+    public bool CkArtistIsChecked { get => _ckArtistIsChecked; set => SetProperty(ref _ckArtistIsChecked, value); }
+
+    private bool _ckAlbumArtistIsChecked;
+    public bool CkAlbumArtistIsChecked { get => _ckAlbumArtistIsChecked; set => SetProperty(ref _ckAlbumArtistIsChecked, value); }
+
+    private bool _ckGenreIsChecked;
+    public bool CkGenreIsChecked { get => _ckGenreIsChecked; set => SetProperty(ref _ckGenreIsChecked, value); }
+
+    private bool _ckAlbumIsChecked;
+    public bool CkAlbumIsChecked { get => _ckAlbumIsChecked; set => SetProperty(ref _ckAlbumIsChecked, value); }
+
+    private bool _ckTitleIsChecked;
+    public bool CkTitleIsChecked { get => _ckTitleIsChecked; set => SetProperty(ref _ckTitleIsChecked, value); }
+
+    private bool _ckYearIsChecked;
+    public bool CkYearIsChecked { get => _ckYearIsChecked; set => SetProperty(ref _ckYearIsChecked, value); }
+
+    private bool _ckBPMIsChecked;
+    public bool CkBPMIsChecked { get => _ckBPMIsChecked; set => SetProperty(ref _ckBPMIsChecked, value); }
+
+    private bool _ckConductorIsChecked;
+    public bool CkConductorIsChecked { get => _ckConductorIsChecked; set => SetProperty(ref _ckConductorIsChecked, value); }
+
+    private bool _ckComposerIsChecked;
+    public bool CkComposerIsChecked { get => _ckComposerIsChecked; set => SetProperty(ref _ckComposerIsChecked, value); }
+
+    private bool _ckInterpretedByIsChecked;
+    public bool CkInterpretedByIsChecked { get => _ckInterpretedByIsChecked; set => SetProperty(ref _ckInterpretedByIsChecked, value); }
+
+    private bool _ckTextWriterIsChecked;
+    public bool CkTextWriterIsChecked { get => _ckTextWriterIsChecked; set => SetProperty(ref _ckTextWriterIsChecked, value); }
+
+    private bool _ckPublisherIsChecked;
+    public bool CkPublisherIsChecked { get => _ckPublisherIsChecked; set => SetProperty(ref _ckPublisherIsChecked, value); }
+
+    private bool _ckEncodedByIsChecked;
+    public bool CkEncodedByIsChecked { get => _ckEncodedByIsChecked; set => SetProperty(ref _ckEncodedByIsChecked, value); }
+
+    private bool _ckCopyrightIsChecked;
+    public bool CkCopyrightIsChecked { get => _ckCopyrightIsChecked; set => SetProperty(ref _ckCopyrightIsChecked, value); }
+
+    private bool _ckContentGroupIsChecked;
+    public bool CkContentGroupIsChecked { get => _ckContentGroupIsChecked; set => SetProperty(ref _ckContentGroupIsChecked, value); }
+
+    private bool _ckSubTitleIsChecked;
+    public bool CkSubTitleIsChecked { get => _ckSubTitleIsChecked; set => SetProperty(ref _ckSubTitleIsChecked, value); }
+
+    private bool _ckArtistSortIsChecked;
+    public bool CkArtistSortIsChecked { get => _ckArtistSortIsChecked; set => SetProperty(ref _ckArtistSortIsChecked, value); }
+
+    private bool _ckAlbumSortIsChecked;
+    public bool CkAlbumSortIsChecked { get => _ckAlbumSortIsChecked; set => SetProperty(ref _ckAlbumSortIsChecked, value); }
+
+    private bool _ckTitleSortIsChecked;
+    public bool CkTitleSortIsChecked { get => _ckTitleSortIsChecked; set => SetProperty(ref _ckTitleSortIsChecked, value); }
+
+    private bool _ckOriginalAlbumIsChecked;
+    public bool CkOriginalAlbumIsChecked { get => _ckOriginalAlbumIsChecked; set => SetProperty(ref _ckOriginalAlbumIsChecked, value); }
+
+    private bool _ckOriginalArtistIsChecked;
+    public bool CkOriginalArtistIsChecked { get => _ckOriginalArtistIsChecked; set => SetProperty(ref _ckOriginalArtistIsChecked, value); }
+
+    private bool _ckOriginalFileNameIsChecked;
+    public bool CkOriginalFileNameIsChecked { get => _ckOriginalFileNameIsChecked; set => SetProperty(ref _ckOriginalFileNameIsChecked, value); }
+
+    private bool _ckOriginalLyricsWriterIsChecked;
+    public bool CkOriginalLyricsWriterIsChecked { get => _ckOriginalLyricsWriterIsChecked; set => SetProperty(ref _ckOriginalLyricsWriterIsChecked, value); }
+
+    private bool _ckOriginalOwnerIsChecked;
+    public bool CkOriginalOwnerIsChecked { get => _ckOriginalOwnerIsChecked; set => SetProperty(ref _ckOriginalOwnerIsChecked, value); }
+
+    private bool _ckOriginalReleaseIsChecked;
+    public bool CkOriginalReleaseIsChecked { get => _ckOriginalReleaseIsChecked; set => SetProperty(ref _ckOriginalReleaseIsChecked, value); }
+
+    private bool _ckCopyrightUrlIsChecked;
+    public bool CkCopyrightUrlIsChecked { get => _ckCopyrightUrlIsChecked; set => SetProperty(ref _ckCopyrightUrlIsChecked, value); }
+
+    private bool _ckOfficialAudioFileUrlIsChecked;
+    public bool CkOfficialAudioFileUrlIsChecked { get => _ckOfficialAudioFileUrlIsChecked; set => SetProperty(ref _ckOfficialAudioFileUrlIsChecked, value); }
+
+    private bool _ckOfficialArtistUrlIsChecked;
+    public bool CkOfficialArtistUrlIsChecked { get => _ckOfficialArtistUrlIsChecked; set => SetProperty(ref _ckOfficialArtistUrlIsChecked, value); }
+
+    private bool _ckOfficialAudioSourceUrlIsChecked;
+    public bool CkOfficialAudioSourceUrlIsChecked { get => _ckOfficialAudioSourceUrlIsChecked; set => SetProperty(ref _ckOfficialAudioSourceUrlIsChecked, value); }
+
+    private bool _ckOfficialInternetRadioUrlIsChecked;
+    public bool CkOfficialInternetRadioUrlIsChecked { get => _ckOfficialInternetRadioUrlIsChecked; set => SetProperty(ref _ckOfficialInternetRadioUrlIsChecked, value); }
+
+    private bool _ckOfficialPaymentUrlIsChecked;
+    public bool CkOfficialPaymentUrlIsChecked { get => _ckOfficialPaymentUrlIsChecked; set => SetProperty(ref _ckOfficialPaymentUrlIsChecked, value); }
+
+    private bool _ckCommercialInformationUrlIsChecked;
+    public bool CkCommercialInformationUrlIsChecked { get => _ckCommercialInformationUrlIsChecked; set => SetProperty(ref _ckCommercialInformationUrlIsChecked, value); }
+
+    private bool _ckOfficialPublisherUrlIsChecked;
+    public bool CkOfficialPublisherUrlIsChecked { get => _ckOfficialPublisherUrlIsChecked; set => SetProperty(ref _ckOfficialPublisherUrlIsChecked, value); }
+
+    private bool _ckInvolvedPersonIsChecked;
+    public bool CkInvolvedPersonIsChecked { get => _ckInvolvedPersonIsChecked; set => SetProperty(ref _ckInvolvedPersonIsChecked, value); }
+
+    private bool _ckInvolvedMusicianIsChecked;
+    public bool CkInvolvedMusicianIsChecked { get => _ckInvolvedMusicianIsChecked; set => SetProperty(ref _ckInvolvedMusicianIsChecked, value); }
+
+    private bool _ckRemoveExistingRatingsIsChecked;
+    public bool CkRemoveExistingRatingsIsChecked { get => _ckRemoveExistingRatingsIsChecked; set => SetProperty(ref _ckRemoveExistingRatingsIsChecked, value); }
+
+    private bool _ckRemoveLyricsIsChecked;
+    public bool CkRemoveLyricsIsChecked { get => _ckRemoveLyricsIsChecked; set => SetProperty(ref _ckRemoveLyricsIsChecked, value); }
+
+    private bool _ckMediaTypeIsChecked;
+    public bool CkMediaTypeIsChecked { get => _ckMediaTypeIsChecked; set => SetProperty(ref _ckMediaTypeIsChecked, value); }
+
+    private bool _ckTrackLengthIsChecked;
+    public bool CkTrackLengthIsChecked { get => _ckTrackLengthIsChecked; set => SetProperty(ref _ckTrackLengthIsChecked, value); }
 
     #endregion
 
@@ -87,23 +171,62 @@ namespace MPTagThat.TagEdit.ViewModels
     public TagEditViewModel()
     {
       _applyEditCommand = new BaseCommand(ApplyEdit);
+      _textChangedCommand = new BaseCommand(TextChaged);
     }
 
     #endregion
 
     #region Commands
 
+    private ICommand _textChangedCommand;
+
+    public ICommand TextChangedCommand => _textChangedCommand;
+
+    private void TextChaged(object param)
+    {
+      if (!MultiCheckBoxVisibility)
+      {
+        return;
+      }
+
+      var tb = (TextBox)(param as TextChangedEventArgs).Source;
+      if (tb.Name.ToLower() == "tracknumber") CkTrackIsChecked = true;
+      else if (tb.Name.ToLower() == "trackcount") CkTrackIsChecked = true;
+      else if (tb.Name.ToLower() == "discnumber") CkDiscIsChecked = true;
+      else if (tb.Name.ToLower() == "disccount") CkDiscIsChecked = true;
+      else if (tb.Name.ToLower() == "artist") CkArtistIsChecked = true;
+      else if (tb.Name.ToLower() == "albumartist") CkAlbumArtistIsChecked = true;
+
+    }
+
+
     private ICommand _applyEditCommand;
     public ICommand ApplyEditCommand => _applyEditCommand;
 
     private void ApplyEdit(object param)
     {
+      var songEdit = (SongData)param;
+
+      if (_songs == null)
+      {
+        return;
+      }
+
+      foreach (var song in _songs)
+      {
+        song.Changed = true;
+        song.Artist = songEdit.Artist;
+      }
+
+
+      /*
       var evt = new GenericEvent
       {
         Action = "applytagedit"
       };
       evt.MessageData.Add("tags", (SongData)param);
       EventSystem.Publish(evt);
+      */
     }
 
     #endregion
@@ -119,12 +242,9 @@ namespace MPTagThat.TagEdit.ViewModels
       if (songs.Count == 1)
       {
         SongEdit = songs[0];
+        GetFrontCover();
         return;
       }
-
-      // We have multiple Songs selected, so show the Checkboxes and
-      // decide if they shoud be checked.
-      MultiCheckBoxVisibility = true;
 
       SongEdit = new SongData();
       var i = 0;
@@ -138,7 +258,16 @@ namespace MPTagThat.TagEdit.ViewModels
         i++;
       }
 
+      // We have multiple Songs selected, so show the Checkboxes and
+      // decide if they shoud be checked.
+      MultiCheckBoxVisibility = true;
+    }
 
+    private void GetFrontCover()
+    {
+      if (_songEdit.Pictures.Count > 0)
+      {
+      }
     }
 
     #endregion
@@ -148,12 +277,12 @@ namespace MPTagThat.TagEdit.ViewModels
 
     public void OnNavigatedTo(NavigationContext navigationContext)
     {
-      //MultiCheckBoxVisibility = false;
-      var songs = navigationContext.Parameters["songs"] as List<SongData>;
+      MultiCheckBoxVisibility = false;
+      _songs = navigationContext.Parameters["songs"] as List<SongData>;
 
-      if (songs.Count > 0)
+      if (_songs.Count > 0)
       {
-        SetFormBindings(ref songs);
+        SetFormBindings(ref _songs);
       }
     }
 
