8a2a741cff4ec417dfeffdb93e4212b3c0ef4cd1
Helmut Wahrmann <hwahrmann@team-mediaportal.com>
2020-04-28 10:48:52 +0200

    Updated Assembly Copyright

--- MPTagThat/Source/UI/MPTagThat.SongGrid/Properties/AssemblyInfo.cs
+++ MPTagThat/Source/UI/MPTagThat.SongGrid/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MPTagThat.SongGrid")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
